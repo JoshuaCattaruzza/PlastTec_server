684bd2e18e4e28cceb073ebdaea50694b09cd051
joshua <joshua.cattaruzza@gmail.com>
2022-05-15 14:19:16 +0200

    dialogflow

--- dialogflow/index.js
+++ dialogflow/index.js
@@ -10,14 +10,14 @@ createTaskIntent.intent('CreaNuovaTask', (conv, param, context) => {
     console.log(param);
     Task.expiry_date = param.date;
     console.log(Task);
-    conv.ask("Task created");
+    conv.ask("Task created, what would you like to call it?");
    });
 createTaskIntent.intent('CreaNuovaTaskAddName', (conv, param, context) => {
     console.log(param);
     Task.name = param.taskname;
     Task.description = param.taskdescription;
 
-    conv.ask("Added task name and description");
+    conv.ask("Added task name and description, who should I assign the task to?");
 
    });
 createTaskIntent.intent('CreaNuovaTaskAddAssignee', (conv, param, context) => {
@@ -25,7 +25,13 @@ createTaskIntent.intent('CreaNuovaTaskAddAssignee', (conv, param, context) => {
     Task.assignee = param.person.name;
     const inputData = new taskModel(Task);
     console.log(inputData);
-    conv.ask("Added assignee");
+    conv.ask(`Assigned task to ${param.person.name}`);
+    inputData
+		.save()
+		.then(()=> { res.status(200).send({message: "task created"})})
+		.catch((err) => {
+			 res.status(400).send(err);
+		});
 
    });
 
