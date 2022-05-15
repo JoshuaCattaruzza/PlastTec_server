d816fce31fc416426c67d3dc4a2f269406582637
joshua <joshua.cattaruzza@gmail.com>
2022-05-15 13:53:32 +0200

    dialogflow

--- dialogflow/index.js
+++ dialogflow/index.js
@@ -4,13 +4,31 @@ const taskModel = require('../models/taskModel');
 
 const createTaskIntent = dialogflow({debug: false});
 
-createTaskIntent.intent('CreaNuovaTaskAddAssignee', (conv, param, context) => {
-    console.log("conv: ")
-    console.log(conv);
-    console.log("param: ");
+createTaskIntent.intent('CreaNuovaTask', (conv, param, context) => {
+    // console.log("conv: ")
+    // console.log(conv);
+    // console.log("param: ");
     console.log(param);
-    console.log("context: " + context);
-    conv.ask("Ho creato una task");
+    // console.log("context: " + context);
+    conv.ask("Task created");
+
+   });
+createTaskIntent.intent('CreaNuovaTaskAddName', (conv, param, context) => {
+    // console.log("conv: ")
+    // console.log(conv);
+    // console.log("param: ");
+    console.log(param);
+    // console.log("context: " + context);
+    conv.ask("Added task name and description");
+
+   });
+createTaskIntent.intent('CreaNuovaTaskAddAssignee', (conv, param, context) => {
+    // console.log("conv: ")
+    // console.log(conv);
+    // console.log("param: ");
+    console.log(param);
+    // console.log("context: " + context);
+    conv.ask("Added assignee");
 
    });
 
